a1835a44ce0d4bb037c47cb4979fb5214218c612
Nanatsuu <anxkuri@gmail.com>
2023-09-10 00:46:27 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,30 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <h1>Calculate alcohol blood level</h1>
+    
+    <label for="weight">Weight:</label>
+    <input type="number" id="weight"><br /><br /></input>
+
+    <label for="bottles">Bottles:</label>
+    <input type="number" id="bottles"><br /><br /></input>
+
+    <label for="time">Time:</label>
+    <input type="number" id="time"><br /><br /></input>
+    <div>
+        <label className="gender-selection" for="male">Male</label>
+        <input type="radio" id="male" name="gender" value="male"></input>
+        
+        <label className="gender-selection" for="female">Female</label>
+        <input type="radio" id="female" name="gender" value="female"></input>
+    </div><br /><br />
+
+
+    <button onclick="calculate()">Calculate</button><br><br></br></br>
+
+    <div id="result"></div>
+</div>
   );
 }
 
